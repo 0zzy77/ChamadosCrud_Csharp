60ec6f7e4f6480303b4ce5a0598e45ed9f4ecf3b
0zzy77 <0zzy.ctf@gmail.com>
2025-07-21 15:28:50 -0300

    feat: criação do select no método index de Location e ajuste na view

--- ChamadosCRUD/Controllers/LocationController.cs
+++ ChamadosCRUD/Controllers/LocationController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChamadosCRUD.Data;
+using ChamadosCRUD.Models;
 
 namespace ChamadosCRUD.Controllers
 {
     public class LocationController : Controller
     {
-        public IActionResult Index()
+        private readonly ChamadosCRUDContext _context;
+
+        public LocationController(ChamadosCRUDContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            return View(await _context.Location.ToListAsync());
         }
     }
 }
