ba58825fed710405e3475118ef6efc22de17ed69
Erllan da Silva Rego <46660@pmsp.local>
2025-07-21 09:07:08 -0300

    criacao projeto e views iniciais

---
+++ ChamadosCRUD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using ChamadosCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChamadosCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
