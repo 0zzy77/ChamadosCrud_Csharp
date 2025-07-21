5e294dc3664d1060e9587fe97c46089c2689b00b
0zzy77 <0zzy.ctf@gmail.com>
2025-07-21 10:25:21 -0300

    feat: location model e adição do git ignore

---
+++ ChamadosCRUD/Models/Location.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChamadosCRUD.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Column(TypeName = "LONGTEXT")]
+        public string? Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        [Required]
+        public string CEP { get; set; }
+        [Required]
+        public string Rua { get; set; }
+        [Required]
+        public string Bairro { get; set; }
+    }
+}
